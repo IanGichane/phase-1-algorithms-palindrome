36c6bd7d987699b251a22af2839c71975102442e
IanGichane <gichaneian@gmail.com>
2023-10-08 11:48:56 +0300

    revied two ways to check if the string is a palindrom index1 and index 2

--- index2.js
+++ index2.js
@@ -1,5 +1,19 @@
-function is palindrome(str){
-//need to split string
-let rts = str
-}
-isPalindrome('hello')
+function isPalindrome(str){
+    //need to split string
+    console.log(str)
+    // let rts = str.split('')
+    // console.log(rts)
+    // rts.reverse()
+    // console.log(rts)
+    // let joined = rts.join('')
+    let rts=str.split('').reverse().join('')
+    //console.log(joined)
+     if (str === rts) {
+            console.log(str + ' ispalindrome')
+            
+          } else {
+            console.log(str +' isNOTpalindrome')
+              return false;
+        }
+    }
+    isPalindrome('i nun i')
